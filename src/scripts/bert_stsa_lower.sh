0cca94174e2601204394965e1fcdfce7efa64341
sopolat <suleyman.olcay.polat@gmail.com>
2023-02-19 20:11:53 -0600

    Update bert_stsa_lower.sh

--- src/scripts/bert_stsa_lower.sh
+++ src/scripts/bert_stsa_lower.sh
@@ -62,30 +62,6 @@ do
     cp $RAWDATADIR/dev.tsv $CBERTDIR/dev.tsv
     python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $CBERTDIR --seed ${i} --learning_rate $BERTLR  --cache $CACHE > $RAWDATADIR/bert_cbert.log
 
-   # #######################
-   # # CMODBERT Classifier
-   # ######################
-
-    CMODBERTDIR=$RAWDATADIR/cmodbert
-    mkdir $CMODBERTDIR
-    python $SRC/bert_aug/cmodbert.py --data_dir $RAWDATADIR --output_dir $CMODBERTDIR --task_name $TASK  --num_train_epochs 150 --learning_rate 0.00015 --seed ${i} --cache $CACHE > $RAWDATADIR/cmodbert.log
-    cat $RAWDATADIR/train.tsv $CMODBERTDIR/cmodbert_aug.tsv > $CMODBERTDIR/train.tsv
-    cp $RAWDATADIR/test.tsv $CMODBERTDIR/test.tsv
-    cp $RAWDATADIR/dev.tsv $CMODBERTDIR/dev.tsv
-    python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $CMODBERTDIR --seed ${i} --learning_rate $BERTLR  --cache $CACHE > $RAWDATADIR/bert_cmodbert.log
-
-   # #######################
-   # # CMODBERTP Classifier
-   # ######################
-
-    CMODBERTPDIR=$RAWDATADIR/cmodbertp
-    mkdir $CMODBERTPDIR
-    python $SRC/bert_aug/cmodbertp.py --data_dir $RAWDATADIR --output_dir $CMODBERTPDIR --task_name $TASK  --num_train_epochs 10 --seed ${i} --cache $CACHE > $RAWDATADIR/cmodbertp.log
-    cat $RAWDATADIR/train.tsv $CMODBERTPDIR/cmodbertp_aug.tsv > $CMODBERTPDIR/train.tsv
-    cp $RAWDATADIR/test.tsv $CMODBERTPDIR/test.tsv
-    cp $RAWDATADIR/dev.tsv $CMODBERTPDIR/dev.tsv
-    python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $CMODBERTPDIR --seed ${i} --learning_rate $BERTLR   --cache $CACHE > $RAWDATADIR/bert_cmodbertp.log
-
     done
 done
 
